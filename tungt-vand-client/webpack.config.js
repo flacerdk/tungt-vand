3a43d0a832a6c8aec59a5242e49b4b0ecf908fb5
Felipe Lacerda <fegolac@gmail.com>
2016-12-14 10:52:34 +0100

    Rename index file

--- tungt-vand-client/webpack.config.js
+++ tungt-vand-client/webpack.config.js
@@ -5,7 +5,7 @@ const BUILD_DIR = path.join(__dirname, 'src')
 const OUTPUT_DIR = path.join(__dirname, '../public/javascripts/')
 
 module.exports = {
-    entry: path.join(BUILD_DIR, 'index.js'),
+    entry: path.join(BUILD_DIR, 'index.jsx'),
     output: {
         path: OUTPUT_DIR,
         filename: '[name].js',
